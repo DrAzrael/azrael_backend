ea653ecfe43888d6e22e76cc3638a91b9df49748
DrAzrael <u8_aleszl_lbn@technischools.com>
2024-11-13 18:04:56 +0100

    jebane corsy
    naprawione dziaaj

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// const cors = require('cors') //to potrzeba przy łączeniu z frontem pamientaj
+const cors = require('cors') //to potrzeba przy łączeniu z frontem pamientaj
 
 
 
